ab634e413f1ff8ca7239f3373bf6be190ac1d377
rafael <rafaelreyesgarcia93@gmail.com>
2022-11-10 16:10:48 -0600

    finished tutorial

--- front-end/toy-projects/javascript30/08 - Fun with HTML5 Canvas/script.js
+++ front-end/toy-projects/javascript30/08 - Fun with HTML5 Canvas/script.js
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>HTML5 Canvas</title>
-</head>
-<body>
-<canvas id="draw" width="800" height="800"></canvas>
-<script>
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
+
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height =  window.innerHeight;
+
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
@@ -20,26 +13,28 @@ ctx.lineWidth = 100;
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+
 let hue = 0;
 let direction = true;
 
 function draw(e) {
-  if (!isDrawing) return; // stop the fn from running when they are not moused down
-  console.log(e);
+  if(!isDrawing) return; //stops the fn from running when they are not mousedown
+
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
-  // start from
   ctx.moveTo(lastX, lastY);
-  // go to
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
+
+  // lastX = e.offsetX;
+  // lastY = e.offsetY;
   [lastX, lastY] = [e.offsetX, e.offsetY];
 
   hue++;
   if (hue >= 360) {
     hue = 0;
   }
-  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+  if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
     direction = !direction;
   }
 
@@ -48,7 +43,6 @@ function draw(e) {
   } else {
     ctx.lineWidth--;
   }
-
 }
 
 canvas.addEventListener('mousedown', (e) => {
@@ -56,18 +50,7 @@ canvas.addEventListener('mousedown', (e) => {
   [lastX, lastY] = [e.offsetX, e.offsetY];
 });
 
-
 canvas.addEventListener('mousemove', draw);
+
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
-
-</script>
-
-<style>
-  html, body {
-    margin: 0;
-  }
-</style>
-
-</body>
-</html>
