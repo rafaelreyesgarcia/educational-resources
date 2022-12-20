ff856141bbafe02bb7737547856cb2f190ddb56b
rafael <rafaelreyesgarcia93@gmail.com>
2022-12-20 10:03:55 -0600

    removed ECDSA node project files

--- blockchain/Ethereum/alchemy-eth-bootcamp/1.blockchain-cryptography/ethereum-cryptography/exercises/3.RecoverKey.js
+++
@@ -1,6 +0,0 @@
-const secp = require("ethereum-cryptography/secp256k1");
-const hashMessage = require("./2.hashMessage");
-
-async function recoverKey(message, signature, recoveryBit) {
-  return secp.recoverPublicKey(hashMessage(message), signature, recoveryBit);
-}
